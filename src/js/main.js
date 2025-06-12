98b84ae79f266b0f48882ce79ec2743456dd4d01
Renato Gregorio <renatogregorio@protonmail.com>
2025-06-12 09:39:12 -0300

    fix: correcao no css html e js

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,10 @@
-$('.carousel').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      adaptiveHeight: true
-    });
-
     const inicio = new Date("2025-02-18T00:00:00").getTime();
     setInterval(() => {
       const agora = new Date().getTime();
       let distancia = agora - inicio;
 
-      const anos = Math.floor(distancia / (1000 * 60 * 60 * 24 * 365.25));
-      distancia -= anos * (1000 * 60 * 60 * 24 * 365.25);
-
       const meses = Math.floor(distancia / (1000 * 60 * 60 * 24 * 30.44));
       distancia -= meses * (1000 * 60 * 60 * 24 * 30.44);
-
-      const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
-      distancia -= dias * (1000 * 60 * 60 * 24);
-
-      const horas = Math.floor(distancia / (1000 * 60 * 60));
-      distancia -= horas * (1000 * 60 * 60);
-
-      const minutos = Math.floor(distancia / (1000 * 60));
-      distancia -= minutos * (1000 * 60);
-
-      const segundos = Math.floor(distancia / 1000);
-
    
       document.getElementById("meses").textContent = String(meses).padStart(2, '0');
       
